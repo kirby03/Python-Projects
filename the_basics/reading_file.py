e412e5af35471142f87c107e668ee3833dcefa50
kirby03 <backoff521@gmail.com>
2020-12-03 01:38:53 +0400

    more practice

--- the_basics/reading_file.py
+++ the_basics/reading_file.py
@@ -1,2 +1,23 @@
-myfile = open("fruits.txt")
-print(myfile.read())
+# read once only due to cursor position at end of last text
+# myfile = open("fruits.txt")
+# print(myfile.read())
+# print(myfile.read()) - empty string
+
+# print multiple times, store in variable the read
+# myfile = open("fruits.txt")
+# myfile = open('fruits.txt')
+# content = myfile.read()
+# close to delte temp memory
+# myfile.close()
+
+with open('fruits.txt', "r+") as f:
+    content = f.read()
+    f.write("\n" + content + "\n" + content)
+    content = f.seek(0)
+    content2 = f.read()
+    print(content2)
+
+
+
+
+
